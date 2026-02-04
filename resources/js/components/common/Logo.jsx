be214e3c01a60d7b47c0050232d7b99621a71665
elkhattaby0 <eelkhattaby@gmail.com>
2026-02-04 02:14:07 +0100

    v1.1

--- resources/js/components/common/Logo.jsx
+++ resources/js/components/common/Logo.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
-    <Link to="/" className="Wordmark">
-      matzapich
+    <Link to="/" className="logo-matzapich logo-high-x-height">
+      mat<span className="wordmark-z">z</span>apich
     </Link>
   );
 }
